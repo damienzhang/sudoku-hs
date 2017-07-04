56b6e197b14567611a6917596deae7f750611d77
Damien Zhang <zhang.damien@gmail.com>
2017-07-04 15:01:53 -0400

    Reformatting for better web formatting

--- src/Main.hs
+++ src/Main.hs
@@ -11,15 +11,15 @@ import Data.Set (Set, (\\))
 import SudokuTypes
 
 data PreSet = PreSet
-    { psNumbers :: !IntSet
-    , psCells :: !(Set Idx)
-    }
+  { psNumbers :: !IntSet
+  , psCells :: !(Set Idx)
+  }
 
 instance Show PreSet where
-    show ps =
-        "{Numbers=" ++
-        (show . IntSet.toList . psNumbers) ps ++
-        ", cells=" ++ (show . Set.toList . psCells) ps ++ "}"
+  show ps =
+    "{Numbers=" ++
+    (show . IntSet.toList . psNumbers) ps ++
+    ", cells=" ++ (show . Set.toList . psCells) ps ++ "}"
 
 type BoardState a = StateT Board Maybe a -- Board -> Maybe (a, Board)
 
@@ -28,55 +28,52 @@ readCell ij = StateT $ \board -> Just (boardGetCell board ij, board)
 
 writeCell :: Idx -> Maybe Cell -> BoardState ()
 writeCell ij cell =
-    StateT $ \board -> do
-        c <- cell
-        let newBoard = boardSetCell board ij c
-        case newBoard of
-            Nothing -> Nothing
-            Just b -> Just ((), b)
+  StateT $ \board -> do
+    c <- cell
+    let newBoard = boardSetCell board ij c
+    case newBoard of
+      Nothing -> Nothing
+      Just b -> Just ((), b)
 
 crossOut
-    :: Foldable f
-    => f Idx -> IntSet -> BoardState ()
+  :: Foldable f
+  => f Idx -> IntSet -> BoardState ()
 crossOut ijs values =
-    forM_ ijs $ \ij -> do
-        cell <- readCell ij
-        writeCell ij (deleteCellValues values cell)
+  forM_ ijs $ \ij ->
+    readCell ij >>= return . deleteCellValues values >>= writeCell ij
+    -- cell <-
+    -- writeCell ij ()
 
 markup :: Board -> Maybe Board
-markup = execStateT doMarkup
-  where
-    doMarkup = do
-        board <- get
-        forM_ (boardCells board) $ \(ij, cell) ->
-            case cell of
-                Solved v -> do
-                    let peers =
-                            filter (/= ij) $ colOf ij ++ rowOf ij ++ boxOf ij
-                    crossOut peers (IntSet.singleton v)
-                _ -> return ()
+markup board =
+  flip execStateT board $
+  forM_ (boardCells board) $ \(ij, cell) -> do
+    when (cellIsSolved cell) $ do
+      let Solved n = cell
+      let peers = filter (/= ij) $ colOf ij ++ rowOf ij ++ boxOf ij
+      crossOut peers (IntSet.singleton n)
 
 applyPreSets :: Board -> Maybe Board
 applyPreSets board =
-    flip execStateT board $ do
-        go rows
-        go cols
-        go boxes
+  flip execStateT board $ do
+    go rows
+    go cols
+    go boxes
   where
     go ranges =
-        forM_ ranges $ \ijs -> do
-            b <- get
-            forM_ (preSets b ijs) $ \ps -> do
-                let otherCells = Set.fromList ijs \\ psCells ps
-                crossOut otherCells (psNumbers ps)
+      forM_ ranges $ \ijs -> do
+        b <- get
+        forM_ (preSets b ijs) $ \ps -> do
+          let otherCells = Set.fromList ijs \\ psCells ps
+          crossOut otherCells (psNumbers ps)
 
 combinations :: Int -> [a] -> [[a]]
 combinations 0 _ = [[]]
 combinations n xs = [y : ys | y:xs' <- tails xs, ys <- combinations (n - 1) xs']
 
 removeAny
-    :: Eq a
-    => [a] -> [[a]] -> [[a]]
+  :: Eq a
+  => [a] -> [[a]] -> [[a]]
 removeAny xs = filter (\ys -> not . or $ (==) <$> xs <*> ys)
 
 findPreSets :: Board -> Int -> [Idx] -> [PreSet]
@@ -85,17 +82,16 @@ findPreSets board n ijs = go (combinations n unsolvedCells) []
     unsolvedCells = filter (cellIsUnsolved . boardGetCell board) ijs
     go [] acc = acc
     go (x:xs) acc =
-        let nums =
-                foldr
-                    (IntSet.union . unsolvedCellSet . boardGetCell board)
-                    IntSet.empty
-                    x
-        in if IntSet.size nums == n
-               then go
-                        (removeAny x xs)
-                        (PreSet {psNumbers = nums, psCells = Set.fromList x} :
-                         acc)
-               else go xs acc
+      let nums =
+            foldr
+              (IntSet.union . unsolvedCellSet . boardGetCell board)
+              IntSet.empty
+              x
+      in if IntSet.size nums == n
+           then go
+                  (removeAny x xs)
+                  (PreSet {psNumbers = nums, psCells = Set.fromList x} : acc)
+           else go xs acc
 
 preSets :: Board -> [Idx] -> [PreSet]
 preSets board ijs = ps 2 ++ ps 3 ++ ps 4
@@ -104,36 +100,36 @@ preSets board ijs = ps 2 ++ ps 3 ++ ps 4
 
 chooseUnsolvedCell :: Board -> (Idx, Cell)
 chooseUnsolvedCell board =
-    List.minimumBy (\(_, x) (_, y) -> compare (cellSize x) (cellSize y)) $
-    filter (cellIsUnsolved . snd) . boardCells $ board
+  List.minimumBy (\(_, x) (_, y) -> compare (cellSize x) (cellSize y)) $
+  filter (cellIsUnsolved . snd) . boardCells $ board
 
 simplify :: Board -> Maybe Board
 simplify board = go (Just board) Nothing
   where
     go a b
-        | a == b = a
-        | otherwise = go (a >>= markup >>= applyPreSets) a
+      | a == b = a
+      | otherwise = go (a >>= markup >>= applyPreSets) a
 
 solve :: Board -> Maybe Board
 solve board = do
-    simplifiedBoard <- verify =<< simplify board
-    if boardIsSolved simplifiedBoard
-        then Just simplifiedBoard
-        else do
-            let (ij, cell) = chooseUnsolvedCell simplifiedBoard
-            let solvedBoards =
-                    map
-                        (solve <=< boardSetCellValue simplifiedBoard ij)
-                        (cellValues cell)
-            firstValid solvedBoards
+  simplifiedBoard <- verify =<< simplify board
+  if boardIsSolved simplifiedBoard
+    then Just simplifiedBoard
+    else do
+      let (ij, cell) = chooseUnsolvedCell simplifiedBoard
+      let solvedBoards =
+            map
+              (solve <=< boardSetCellValue simplifiedBoard ij)
+              (cellValues cell)
+      firstValid solvedBoards
   where
     firstValid = asum
 
 main :: IO ()
 main = do
-    line <- getLine
-    let solution = join (solve <$> parseBoard line)
-    case solution of
-        Just board -> print board
-        Nothing -> putStrLn "Invalid puzzle"
-    main
+  line <- getLine
+  let solution = join (solve <$> parseBoard line)
+  case solution of
+    Just board -> print board
+    Nothing -> putStrLn "Invalid puzzle"
+  main
--- src/SudokuTypes.hs
+++ src/SudokuTypes.hs
@@ -35,16 +35,16 @@ boxOf :: Idx -> [Idx]
 boxOf ij = boxMap Map.! ij
   where
     boxMap =
-        Map.fromList . concatMap (\box -> map (\ij' -> (ij', box)) box) $ boxes
+      Map.fromList . concatMap (\box -> map (\ij' -> (ij', box)) box) $ boxes
 
 data Cell
-    = Unsolved !IntSet
-    | Solved !Int
-    deriving (Eq)
+  = Unsolved !IntSet
+  | Solved !Int
+  deriving (Eq)
 
 instance Show Cell where
-    show (Unsolved s) = concatMap show (IntSet.toList s)
-    show (Solved n) = show n
+  show (Unsolved s) = concatMap show (IntSet.toList s)
+  show (Solved n) = show n
 
 cellIsUnsolved :: Cell -> Bool
 cellIsUnsolved (Unsolved _) = True
@@ -71,34 +71,34 @@ defaultCellSet = Unsolved (IntSet.fromList [1 .. 9])
 
 deleteCellValues :: IntSet -> Cell -> Maybe Cell
 deleteCellValues values (Unsolved s) =
-    let s' = s IntSet.\\ values
-    in case IntSet.size s' of
-           0 -> Nothing
-           1 -> Just $ (Solved . head . IntSet.toList) s'
-           _ -> Just $ Unsolved s'
+  let s' = s IntSet.\\ values
+  in case IntSet.size s' of
+       0 -> Nothing
+       1 -> Just $ (Solved . head . IntSet.toList) s'
+       _ -> Just $ Unsolved s'
 deleteCellValues _ cell = Just cell
 
 newtype Board =
-    Board (Map Idx Cell)
-    deriving (Eq)
+  Board (Map Idx Cell)
+  deriving (Eq)
 
 boardGetCell :: Board -> Idx -> Cell
 boardGetCell (Board cs) ij = cs Map.! ij
 
 instance Show Board where
-    show (Board board) =
-        concat $ mapGroup 9 (\xs -> concat xs ++ "\n") formattedCells
-      where
-        cellText = map show (Map.elems board)
-        cellWidth = (maximum . map length) cellText + 1
-        formattedCells = map (pad cellWidth) cellText
-        pad n x = printf "%*s" n x
-        mapGroup _ _ [] = []
-        mapGroup n f xs = f (take n xs) : mapGroup n f (drop n xs)
+  show (Board board) =
+    concat $ mapGroup 9 (\xs -> concat xs ++ "\n") formattedCells
+    where
+      cellText = map show (Map.elems board)
+      cellWidth = (maximum . map length) cellText + 1
+      formattedCells = map (pad cellWidth) cellText
+      pad n x = printf "%*s" n x
+      mapGroup _ _ [] = []
+      mapGroup n f xs = f (take n xs) : mapGroup n f (drop n xs)
 
 boardIsSolved :: Board -> Bool
 boardIsSolved b@(Board cs) =
-    all cellIsSolved (Map.elems cs) && (isJust . verify) b
+  all cellIsSolved (Map.elems cs) && (isJust . verify) b
 
 boardSetCellValue :: Board -> Idx -> Int -> Maybe Board
 boardSetCellValue board ij n = boardSetCell board ij (Solved n)
@@ -111,23 +111,23 @@ boardCells (Board cs) = Map.toAscList cs
 
 parseBoard :: String -> Maybe Board
 parseBoard puzzle
-    | length cellSets == length cells =
-        verify . Board . Map.fromList $ zip cells cellSets
-    | otherwise = Nothing
+  | length cellSets == length cells =
+    verify . Board . Map.fromList $ zip cells cellSets
+  | otherwise = Nothing
   where
     cellSets = map convertNumber puzzle
     convertNumber n
-        | n `elem` ['1' .. '9'] = Solved (digitToInt n)
-        | otherwise = defaultCellSet
+      | n `elem` ['1' .. '9'] = Solved (digitToInt n)
+      | otherwise = defaultCellSet
 
 verify :: Board -> Maybe Board
 verify board =
-    if all verifyRange (rows ++ cols ++ boxes)
-        then Just board
-        else Nothing
+  if all verifyRange (rows ++ cols ++ boxes)
+    then Just board
+    else Nothing
   where
     verifyRange cs =
-        let solvedCells =
-                filter cellIsSolved $
-                foldr (\a xs -> boardGetCell board a : xs) [] cs
-        in length solvedCells == (length . List.nub) solvedCells
+      let solvedCells =
+            filter cellIsSolved $
+            foldr (\a xs -> boardGetCell board a : xs) [] cs
+      in length solvedCells == (length . List.nub) solvedCells
